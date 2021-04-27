2f03bdf6c56a5d01fdc41650ac2028548a692ad3
abrarfayez <38536654+abrarfayez@users.noreply.github.com>
2021-04-27 22:56:32 +0200

    Update User.java

--- app/src/main/java/com/example/smart_edu/model/User.java
+++ app/src/main/java/com/example/smart_edu/model/User.java
@@ -1,34 +1,15 @@
-package com.example.smart_edu.model;
+package com.example.smartedu.model;
 
 public class User {
-    private int id;
-    private String username;
-    private String password;
-    private String token;
+    private String key;
 
 
-    public User(int id, String username, String password, String token) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.token = token;
-    }
-
-
-    public int getId(){
-        return id;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
+    public User(String key) {
+        this.key = key;
     }
 
     public String getToken(){
-        return token;
+        return key;
     }
 
 }
