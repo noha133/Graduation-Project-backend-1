b39c7048fefe9425acbd5dfbec32df449b2af73e
abrarfayez <38536654+abrarfayez@users.noreply.github.com>
2021-04-27 22:55:33 +0200

    Update PostAPI.java

--- app/src/main/java/com/example/smart_edu/PostAPI.java
+++ app/src/main/java/com/example/smart_edu/PostAPI.java
@@ -1,26 +1,44 @@
-package com.example.smart_edu;
+package com.example.smartedu;
 
-import com.example.smart_edu.model.Login;
-import com.example.smart_edu.model.User;
+import com.example.smartedu.model.Admin_info;
+import com.example.smartedu.model.Login;
+import com.example.smartedu.model.Student_info;
+import com.example.smartedu.model.Teacher_info;
+import com.example.smartedu.model.User;
+import com.example.smartedu.model.Users;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Url;
+
 public interface PostAPI {
-
-
-//    String root = "http://nohamagdy.pythonanywhere.com/";
-////    String root = "http://127.0.0.1:8000/";
-//
-//
-//    String base_local = root + "api/v1/";
-//    String BASE_URL = base_local + "account/";
-//    String POST_URL = base_local + "post/";
-//    String API_URL = root + "api/v1/";
-
-
 
     @POST("dj-rest-auth/login/")
     Call<User> login(@Body Login login);
-}
 
+    @GET("dj-rest-auth/user/")
+    Call<Users> getPost(@Header("Authorization")  String djangoToken);
+
+    @GET// dynamic url
+    Call<UserTypeChecker> gettype(
+            @Url String url
+    );
+
+    @GET// dynamic url
+    Call<Student_info> getStudentInfo(
+        @Url String url
+        );
+
+    @GET// dynamic url
+    Call<Teacher_info> getTeacherInfo(
+            @Url String url
+    );
+
+    @GET// dynamic url
+    Call<Admin_info> getadminInfo(
+            @Url String url
+    );
+}
